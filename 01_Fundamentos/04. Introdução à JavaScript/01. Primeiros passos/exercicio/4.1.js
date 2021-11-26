6a0b4706b75fafdc0d0ecc6b011366bc54e0d341
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-11-26 13:54:04 -0500

    Exercicios 1 - 4 concluidos

---
+++ 01_Fundamentos/04. Introdução à JavaScript/01. Primeiros passos/exercicio/4.1.js
@@ -0,0 +1,40 @@
+var a = 3;
+var b = 5;
+console.log("1): ")
+console.log("Adição de a + b = ", a + b)
+console.log("Subtração de a - b = ", a - b)
+console.log("Multiplicação de a * b = ", a * b)
+console.log("Divisão de a / b = ", a / b)
+console.log("Módulo de a % b = ", a % b)
+console.log(" ")
+console.log("2): ")
+if(a > b){
+    console.log("a é maior q b")
+} else{
+    console.log("b é maior q a")
+}
+console.log(" ")
+console.log("3): ")
+const al = 7;
+const el = 10;
+const il = 5;
+
+if(al > el && al > il){
+    console.log(al +" é maior que "+ el +" e "+il)
+} else if(el > al && el > il){
+    console.log(el +" é maior que "+ al +" e "+il)
+} else{
+    console.log(il +" é maior que "+ al +" e "+el)
+}
+console.log(" ")
+console.log("4): ")
+var num = -1;
+if(num > 0){
+    console.log("positivo")
+} else if(num < 0){
+    console.log("negativo")
+} else if(num === 0){
+    console.log("zero")
+}
+console.log(" ")
+console.log("5): ")
