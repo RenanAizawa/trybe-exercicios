edb5267bc14b17faed8b70309288c8eae4c2e6ae
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-03 21:31:54 -0500

    Modificaçoes no script1

---
+++ 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/01.DOM-e-seletores/script1.js
@@ -0,0 +1,8 @@
+// Body Header
+document.querySelector('#header-container').style.backgroundColor = 'green';
+// Section Div class = urgencia
+var corDaUrgencia = document.querySelectorAll('.emergency-tasks');
+for (var i of corDaUrgencia.length) {
+    i.style.backgroundColor = '#c723b9;'
+}
+    
