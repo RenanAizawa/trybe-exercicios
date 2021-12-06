1ee7186e67bc9c7536d82fc624f71bbdc43ce1d1
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-06 09:34:19 -0500

    finalizado primeiro exercicio do bloco 5.1

--- 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/01.DOM-e-seletores/script1.js
+++ 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/01.DOM-e-seletores/script1.js
@@ -3,6 +3,20 @@ document.querySelector('#header-container').style.backgroundColor = 'green';
 // Section Div class = urgencia
 var corDaUrgencia = document.querySelectorAll('.emergency-tasks div');
 for (var i of corDaUrgencia) {
-    i.style.backgroundColor = '#c723b9';
+    i.style.backgroundColor = '#f37bd9a6';
 }
-    
+var corTituloUrgencia = document.querySelectorAll('.emergency-tasks h3');
+for (var i of corTituloUrgencia) {
+    i.style.backgroundColor = '#ec04baa6';
+}
+// Section Div class = Não-Urgencia
+var corNaoUrgente = document.querySelectorAll('.no-emergency-tasks div');
+for (var i of corNaoUrgente) {
+    i.style.backgroundColor = '#e9f362f5';
+}
+var corTituloNaoUrgencia = document.querySelectorAll('.no-emergency-tasks h3');
+for (var i of corTituloNaoUrgencia) {
+    i.style.backgroundColor = '#0000009a';
+}
+// Footer
+document.querySelector('#footer-container').style.backgroundColor = "green";
