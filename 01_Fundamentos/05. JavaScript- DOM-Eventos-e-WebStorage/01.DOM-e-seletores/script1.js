ae343305e923f00aca6629feec30125c52d5fa3a
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-06 08:52:25 -0500

    Finalemte funcionou

--- 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/01.DOM-e-seletores/script1.js
+++ 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/01.DOM-e-seletores/script1.js
@@ -1,8 +1,8 @@
 // Body Header
 document.querySelector('#header-container').style.backgroundColor = 'green';
 // Section Div class = urgencia
-var corDaUrgencia = document.querySelectorAll('.emergency-tasks');
-for (var i of corDaUrgencia.length) {
-    i.style.backgroundColor = '#c723b9;'
+var corDaUrgencia = document.querySelectorAll('.emergency-tasks div');
+for (var i of corDaUrgencia) {
+    i.style.backgroundColor = '#c723b9';
 }
     
