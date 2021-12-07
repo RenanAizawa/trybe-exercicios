6cb65b66f8c739962ec8db785292570adf9d3a2a
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-07 17:32:42 -0500

    Atividades 2 feita com ajuda

--- 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/03.Eventos/main.js
+++ 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/03.Eventos/main.js
@@ -11,7 +11,14 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
+function addClass (event) {
+    const techClass = document.querySelector('.tech');
+    techClass.classList.remove('tech')
+    event.target.className = 'tech';
+}
+secondLi.addEventListener('click', addClass);
+thirdLi.addEventListener('click', addClass);
+firstLi.addEventListener('click', addClass);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
