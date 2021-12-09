523f7c8a6d3a4518ed555c5dd00b16ad8adeab50
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-09 15:44:17 -0500

    alterações

--- 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/04.Web-Storage/script.js
+++ 01_Fundamentos/05. JavaScript- DOM-Eventos-e-WebStorage/04.Web-Storage/script.js
@@ -1,1 +1,12 @@
 console.log('oi');
+// Variaveis para receber as informações
+const n1 = document.getElementsById('input1');
+const n2 = document.getElementById('input2');
+const adic = document.getElementsByClassName('mais');
+const subt = document.getElementsByClassName('menos');
+const div = document.getElementsByClassName('divide');
+const mult = document.getElementsByClassName('multiplica');
+// Desenvolvimento de funções
+function operation () {
+    
+}
