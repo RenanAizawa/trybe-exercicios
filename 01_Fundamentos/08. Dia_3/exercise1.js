1f15310176341e1273fff1c188cc8b3126a8cf73
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-20 17:20:54 -0500

    Executado as atividades 1 e 2

---
+++ 01_Fundamentos/08. Dia_3/exercise1.js
@@ -0,0 +1,81 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+];
+  
+// Adicione o código do exercício aqui:
+
+//   1 - Crie um array com strings no formato Nome_do_livro - Genero_do_livro - Nome_da_Pessoa_autora
+
+// const search = (array) => {return array.map((book) => {return `${book.name}-${book.genre} - ${book.author.name}`})};
+
+// console.log(search(books));
+
+//  2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+const nameAndAge = (array) => {return array.map((writer) => {
+    return {age: writer.releaseYear - writer.author.birthYear,
+    author: writer.author.name} 
+})};
+console.log(nameAndAge(books).sort((a , b) => a.age - b.age));
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
